c58575f37169fc5b181e10461a13239f962d9dae
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-03-22 15:56:32 -0300

    Ej05 and Ej06 completed

---
+++ Assets/Scripts/Ej06.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej06 : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("6. Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0 en una variable llamada num1 y muestre un mensaje por pantalla indicando el  número es par o el número es impar. Deberá utilizar el operador “módulo” es el caracter  %.");
+        if ((num1 %2) == 0)
+        {
+            Debug.Log("El numero es Par");
+        }
+        else
+        {
+            Debug.Log("El numero es Impar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
